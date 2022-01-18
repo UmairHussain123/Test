b9c5d1f0a9e1f3dbbe6f06905444e7c12721f5ca
UmairHussain123 <umair.umair2d@gmail.com>
2022-01-19 00:55:47 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import axios from "axios";
+import { StatusBar } from "expo-status-bar";
+import { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+useState;
 export default function App() {
+  const [city, setcity] = useState("Karachi");
+  const [country, setCountry] = useState("PAK");
+  const [Fajr, setFajr] = useState("");
+  const [Sunrise, setSunrise] = useState("");
+  const [Dhuhr, setDhuhr] = useState("");
+  const [Asr, setAsr] = useState("");
+  const [Maghrib, setMaghrib] = useState("");
+  const [Isha, setIsha] = useState("");
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get(
+        // `https://api.aladhan.com/v1/timingsByAddress/18-01-2022?address=Karachi,PAK&method=8`
+        `https://api.aladhan.com/v1/timingsByAddress/18-01-2022?address=${city},${country}&method=8`
+      );
+      const resp = res.data.data.timings.Fajr;
+      console.log(resp);
+      const response = resp.date;
+      console.log(response);
+    };
+    getData();
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text></Text>
     </View>
   );
 }
@@ -13,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
